d55072d00af74c4a3820358546b36cd0ab8f45ab
Gary Russell <rugary@amazon.com>
2020-06-16 17:40:42 -0700

    added text

--- app1/src/App.js
+++ app1/src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        app1
+	  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
